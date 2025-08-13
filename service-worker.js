16be3e9292e47cb50ea810f35b85b7f215284024
bijwesachin <48423996+bijwesachin@users.noreply.github.com>
2025-08-12 20:39:27 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ const ASSETS = [
   './icons/icon-512.png'
 ];
 
+
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
